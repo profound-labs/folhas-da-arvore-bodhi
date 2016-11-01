33a15695da12486a6654064b13efbdd491cd1066
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2016-11-01 13:55:56 +0000

    upd

--- helpers/tex2md.sh
+++ helpers/tex2md.sh
@@ -10,6 +10,10 @@ sed 's/\\thai[{]\([^}]\+\)[}]/\\emph{\1}/g' |\
 sed 's/\\speaker[{]\([^}]\+\)[}]/\\emph{\1}/g' |\
 sed 's/\\LaTeX/LaTeX/g' |\
 sed 's/\(\w\)"-\(\w\)/\1-\2/g' |\
+sed 's/\\tocChapterNote{\([^}]\+\)}/\n\\emph{\1}/g' |\
+sed 's/\\QA{\([^}]\+\)}/\\textbf{\1}/g' |\
+sed 's/\\begin{packeditemize}//g' |\
+sed 's/\\end{packeditemize}//g' |\
 pandoc --smart --normalize --from=latex --to=markdown |\
 sed 's/\([^\\]\)\\$/\1\\\\/' > "$DEST"
 
